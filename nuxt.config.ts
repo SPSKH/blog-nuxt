25d92d00c4d187331df7bd8768782600a761401f
cernochmartin <cernochm@outlook.cz>
2023-12-19 18:45:38 +0100

    feat: add theme to 'github-dark'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   modules: ['@nuxt/content'],
   content: {
     highlight: {
-      theme: 'nord',
+      theme: 'github-dark',
       preload: ['js', 'ts', 'vue', 'json', 'bash', 'md']
     },
     experimental: {
